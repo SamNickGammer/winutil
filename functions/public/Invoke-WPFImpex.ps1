905a2b77a0289c473abbed9f26a3895bad0b4e8e
Sam Nick <omprakash121samuni@gmail.com>
2026-02-27 14:32:18 +0530

    Completed

---
+++ functions/public/Invoke-WPFImpex.ps1
@@ -0,0 +1,84 @@
+function Invoke-WPFImpex {
+    <#
+
+    .SYNOPSIS
+        Handles importing and exporting of the checkboxes checked for the tweaks section
+
+    .PARAMETER type
+        Indicates whether to 'import' or 'export'
+
+    .PARAMETER checkbox
+        The checkbox to export to a file or apply the imported file to
+
+    .EXAMPLE
+        Invoke-WPFImpex -type "export"
+
+    #>
+    param(
+        $type,
+        $Config = $null
+    )
+
+    function ConfigDialog {
+        if (!$Config) {
+            switch ($type) {
+                "export" { $FileBrowser = New-Object System.Windows.Forms.SaveFileDialog }
+                "import" { $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog }
+            }
+            $FileBrowser.InitialDirectory = [Environment]::GetFolderPath('Desktop')
+            $FileBrowser.Filter = "JSON Files (*.json)|*.json"
+            $FileBrowser.ShowDialog() | Out-Null
+
+            if ($FileBrowser.FileName -eq "") {
+                return $null
+            } else {
+                return $FileBrowser.FileName
+            }
+        } else {
+            return $Config
+        }
+    }
+
+    switch ($type) {
+        "export" {
+            try {
+                $Config = ConfigDialog
+                if ($Config) {
+                    $allConfs = $sync.selectedApps + $sync.selectedTweaks + $sync.selectedToggles + $sync.selectedFeatures
+                    $jsonFile = $allConfs | ConvertTo-Json
+                    $jsonFile | Out-File $Config -Force
+                    "iex ""& { `$(irm https://christitus.com/win) } -Config '$Config'""" | Set-Clipboard
+                }
+            } catch {
+                Write-Error "An error occurred while exporting: $_"
+            }
+        }
+        "import" {
+            try {
+                $Config = ConfigDialog
+                if ($Config) {
+                    try {
+                        if ($Config -match '^https?://') {
+                            $jsonFile = (Invoke-WebRequest "$Config").Content | ConvertFrom-Json
+                        } else {
+                            $jsonFile = Get-Content $Config | ConvertFrom-Json
+                        }
+                    } catch {
+                        Write-Error "Failed to load the JSON file from the specified path or URL: $_"
+                        return
+                    }
+                    # TODO how to handle old style? detected json type then flatten it in a func?
+                    # $flattenedJson = $jsonFile.PSObject.Properties.Where({ $_.Name -ne "Install" }).ForEach({ $_.Value })
+                    $flattenedJson = $jsonFile
+                    Update-WinUtilSelections -flatJson $flattenedJson
+
+                    if (!$PARAM_NOUI) {
+                        Reset-WPFCheckBoxes -doToggles $true
+                    }
+                }
+            } catch {
+                Write-Error "An error occurred while importing: $_"
+            }
+        }
+    }
+}
